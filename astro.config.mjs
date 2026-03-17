c09bdfb1a55ad068cc6322a25fbc19c2d8a66928
QhjXOO <79450777+QhjXOO@users.noreply.github.com>
2026-03-17 22:05:58 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'server',           // 或 'server'（全动态）
+  adapter: cloudflare({
+    // 可选：imageService: 'cloudflare' 等
+  }),
   integrations: [mdx()],
 });
